d7080c7477abfc2ca4695d81bb57cb170dac3bee
Toor, Tanvir <tatoor@deloitte.com>
2026-07-22 17:04:46 +0530

    Assignments for Week-02 01-async-js medium

--- Week-02/01-async-js/medium/1-file-cleaner.js
+++ Week-02/01-async-js/medium/1-file-cleaner.js
@@ -11,3 +11,10 @@
 // ```
 // hello world my name is raman
 // ```
+
+const fs = require("node:fs");
+
+const fileContent = fs.readFileSync("Week-02/01-async-js/medium/clean-file.txt", "utf-8");
+const updatedString = fileContent.replaceAll(/\s+/g, " "); // the regex checks for concurrent whitespaces
+fs.writeFileSync("Week-02/01-async-js/medium/clean-file.txt", `The previous string was --> ${fileContent} \nThe new clean string is --> ${updatedString}`);
+
--- Week-02/01-async-js/medium/2-clock.js
+++ Week-02/01-async-js/medium/2-clock.js
@@ -1,8 +1,32 @@
 // Using `1-counter.md` or `2-counter.md` from the easy section, can you create a
 // clock that shows you the current machine time?
 
-// Can you make it so that it updates every second, and shows time in the following formats - 
+// Can you make it so that it updates every second, and shows time in the following formats -
 
 //  - HH:MM::SS (Eg. 13:45:23)
-
 //  - HH:MM::SS AM/PM (Eg 01:45:23 PM)
+function currentTime(timestamp) {
+  const date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  console.log(`${hours}:${minutes}:${seconds}`);
+  let amPm = "AM";
+  if (hours > 12) {
+    hours -= 12;
+    amPm = "PM";
+  } else if (hours === 12) {
+    amPm = "PM";
+  }
+  console.log(`${hours}:${minutes}:${seconds} ${amPm}`);
+}
+
+function counter(time, count) {
+  if (count > 60) return;
+  setTimeout(() => {
+    currentTime(time);
+    counter((time += 1000), ++count);
+  }, 1000);
+}
+
+counter(Date.now(), 0);
