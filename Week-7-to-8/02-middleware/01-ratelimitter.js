9e46ffcac7c15b954c4078df6089c423075f1cb6
Toor, Tanvir <tatoor@deloitte.com>
2026-07-25 11:03:02 +0530

    Assignment Week-7-to-8 02-middleware 01-ratelimitter

--- Week-7-to-8/02-middleware/01-ratelimitter.js
+++ Week-7-to-8/02-middleware/01-ratelimitter.js
@@ -1,6 +1,6 @@
 // You have to create a middleware for rate limiting a users request based on their username passed in the header
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Your task is to create a global middleware (app.use) which will
@@ -12,16 +12,37 @@ const app = express();
 // clears every one second
 
 let numberOfRequestsForUser = {};
-setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
+let resetInterval;
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+resetInterval = setInterval(() => {
+  numberOfRequestsForUser = {};
+}, 1000);
+
+app.use((req, res, next) => {
+  const id = req.headers["user-id"];
+  if (!id) return res.status(400).send("header user-id is required");
+  if (numberOfRequestsForUser[id] === undefined) {
+    numberOfRequestsForUser[id] = 0;
+  }
+  const currUserCount = numberOfRequestsForUser[id];
+  if (currUserCount > 5)
+    return res.status(404).send("Too many requests intercepted");
+  numberOfRequestsForUser[id]++;
+  next();
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
 });
+
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
+});
+
+// app.listen(3000, () => {
+//   console.log("App is listening to port: 3000");
+// });
+
+app.resetInterval = resetInterval;
 
 module.exports = app;
