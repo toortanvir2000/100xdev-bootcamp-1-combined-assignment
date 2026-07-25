fca359301792b0eb934e99b4d177df1ebbbf0690
Toor, Tanvir <tatoor@deloitte.com>
2026-07-25 20:14:40 +0530

    Assignment Week-09 03-jwt

--- Week-09/03-jwt/index.js
+++ Week-09/03-jwt/index.js
@@ -1,10 +1,14 @@
+const jwt = require("jsonwebtoken");
+const jwtPassword = "secret_key";
+
 /**
  * Generates a "Short-Lived" JWT that expires in 1 minute.
  * * @param {string} username - The user's email.
  * @returns {string} A JWT that will be invalid after 60 seconds.
  */
 function signShortLivedToken(username) {
-    // Your code here
+  // Your code here
+  return jwt.sign({ username }, jwtPassword, { expiresIn: 60 });
 }
 
 /**
@@ -13,5 +17,16 @@ function signShortLivedToken(username) {
  * @returns {string} Returns "valid", "expired", or "invalid".
  */
 function checkTokenStatus(token) {
-    // Your code here
+  // Your code here
+  return jwt.verify(token, jwtPassword, (err, decoded) => {
+    if ({ ...err }["name"] === "TokenExpiredError") return "expired";
+    if (err || decoded === undefined) return "invalid";
+    return "valid";
+  });
 }
+
+module.exports = {
+  signShortLivedToken,
+  checkTokenStatus,
+  jwtPassword,
+};
